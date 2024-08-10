79d3fb66b5a44ec0745e435b1961fbb9ec104ba0
Yuri Titko <yurititko@gmail.com>
2024-08-10 23:51:30 +0300

    ertert

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    event.preventDefault()
-    document.querySelector('.btn').addEventListener('click', () => {
-        alert("ХУЙ")
-    })
+    // event.preventDefault()
+    // document.querySelector('.btn').addEventListener('click', () => {
+    //     alert("ХУЙ")
+    // })
+
+    // const send = document.querySelector('.send')
+    // const login = document.querySelector('.login')
+    // const password = document.querySelector('.password')
+
+    // send.addEventListener('click', () => {
+    //     console.warn(login.value)
+    //     console.warn(password.value)
+    // })
+
+    fetch('https://github.com/YuriTitko/dfhgdfjkgjkdfgh/blob/main/data.json')
+      .then(response => response.json())
+      .then(json => console.log(json))
+
 })
