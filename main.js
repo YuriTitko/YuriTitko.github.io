5514368d22fa8c9388b5566f416b13825585100e
Yuri Titko <yurititko@gmail.com>
2024-08-10 23:43:49 +0300

    asdasd

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    event.preventDefault()
+    document.querySelector('.btn').addEventListener('click', () => {
+        alert("ХУЙ")
+    })
+})
