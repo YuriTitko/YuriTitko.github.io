620d7ddbb984bce372fa93692fb8b646b9b49d06
Yuri Titko <56803000+YuriTitko@users.noreply.github.com>
2024-08-11 01:55:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     // event.preventDefault()
     // document.querySelector('.btn').addEventListener('click', () => {
-    //     alert("ХУЙ")
+    //     alert("Alert!")
     // })
 
     // const send = document.querySelector('.send')
